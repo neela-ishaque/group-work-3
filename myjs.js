8276c9c741d4504ff0000f9c0ca6839828464d0c
yasmin Ishaque <46652426+neela-ishaque@users.noreply.github.com>
2023-10-20 12:04:25 +0200

    nn

--- myjs.js
+++ myjs.js
@@ -4,7 +4,7 @@ textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='let
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml10 .letter',
+    targets: '.ml10 .letter',                           
     rotateY: [-90, 0],
     duration: 1300,
     delay: (el, i) => 45 * i
